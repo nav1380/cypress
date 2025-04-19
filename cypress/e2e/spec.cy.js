3a04f5fa2a07bc94f005f945c342cf2122cbdf7e
Godwy <godwynnejoshuadimayuga@gmail.com>
2025-04-19 11:10:50 +0800

    added ci/cd

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,15 +7,6 @@ describe('Test Case 16 - Place Order: Login before Checkout', () => {
     cy.visit('https://automationexercise.com');
   });
 
-  it.skip ('should follow these steps', () => {
-    const customer = generateCustomerData()
-    cy.register(customer)
-    cy.enterAccountInfo(customer) 
-    cy.contains('Account Created!')
-    cy.get('[data-qa="continue-button"]').click()
-    cy.verifyName(customer)
-  });
-
   it ('should scroll to the bottom', () => {
     cy.visit('https://automationexercise.com');
     cy.scrollTo('bottom');
