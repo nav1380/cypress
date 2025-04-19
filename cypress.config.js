d8b4724725a2d78e01d4ec2027f77550ba01856b
Godwy <godwynnejoshuadimayuga@gmail.com>
2025-04-19 11:05:48 +0800

    added ci/cd

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "91vq58",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
